98f59f140e95ee148fdbc6985c7db3b47be4db01
Jesus Jaldin <jesusjaldin2002@gmail.com>
2024-11-12 04:14:17 -0400

    Update repositories and services

--- src/main/java/com/med/backend/persistence/repository/DoctorRepository.java
+++ src/main/java/com/med/backend/persistence/repository/DoctorRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface DoctorRepository extends MongoRepository<Doctor, Integer> {
+    Optional<Doctor> findByUserId(int userId);
 
     Optional<Object> findByLicenseNumber(String licenseNumber);
 }
